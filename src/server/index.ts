20d5042de1fc438106fbce3fab956d48512cc9b9
Moises Barillas <moisesrocha717@gmail.com>
2025-09-20 22:40:17 -0600

    elimina el comentario sobre la re-exportación de la implementación generada de doGet en index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,3 @@
-// Re-export the generated doGet implementation. The file is created by the build script.
 export * from './doGet.generated.ts';
 export * from './sheets.ts';
 export * from './gmail.ts';
